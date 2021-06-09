f37ba57bf39bef223cab36f2caa45e0eba705daf
Kelly Lima <keely06@live.com>
2021-06-09 16:18:54 -0300

    fix bug attribution

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -62,6 +63,13 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <MapContainer center={[-23.5389016, -46.912541]} zoom={15}>
+                    <TileLayer 
+                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <Marker position={[-23.5389016, -46.912541]}></Marker>
+                    </MapContainer>
 
                     <div className="field-group">
                         <div className="field">
